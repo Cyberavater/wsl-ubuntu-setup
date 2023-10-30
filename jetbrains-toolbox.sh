ff5a81d170b597d2ae80b5626a4d8783a76845ad
Cyberavater <32187927+Cyberavater@users.noreply.github.com>
2023-10-31 02:36:46 +0600

    .19

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -6,7 +6,6 @@ set -o pipefail
 echo -e "\e[94mFetching the URL of the latest version...\e[39m"
 ARCHIVE_URL=$(curl -s 'https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release' | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
 ARCHIVE_FILENAME=$(basename "$ARCHIVE_URL")
-INSTALL_DIR="/opt"
 
 echo -e "\e[94mDownload and installing $ARCHIVE_FILENAME...\e[39m"
 sudo wget --show-progress -O- "$ARCHIVE_URL" | sudo tar -xzf - --strip-components=1 -C /opt
@@ -21,7 +20,7 @@ sudo apt install -y appimagelauncher
 
 if [ -z "$CI" ]; then
 	echo -e "\e[94mRunning for the first time to set-up...\e[39m"
-	( "$INSTALL_DIR/jetbrains-toolbox" & )
+	( "/opt/jetbrains-toolbox" & )
 	echo -e "\n\e[32mDone! JetBrains Toolbox should now be running, in your application list, and you can run it in terminal as jetbrains-toolbox (ensure that $SYMLINK_DIR is on your PATH)\e[39m\n"
 else
 	echo -e "\n\e[32mDone! Running in a CI -- skipped launching the AppImage.\e[39m\n"
